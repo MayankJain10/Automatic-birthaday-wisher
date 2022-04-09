e6c805cd106cb6af86d7e5f482a3c4695221453b
Mayank Jain <73280608+MayankJain10@users.noreply.github.com>
2022-04-09 13:38:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import imp
+import pandas as pd
+import datetime
+import smtplib
+import os
+
+#ENTER YOUR DETAILS
+GMAIL_ID = 'rockmayank19@gmail.com'
+GMAIL_PSWD = 'Mayank@1004'
+
+
+def sendEmail(to, sub, msg):
+    print(f"Email to {to} send with subject:{sub} and message {msg}")
+    s= smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(GMAIL_ID, GMAIL_PSWD)
+
+    s.sendmail(GMAIL_ID, to,f"Subject:{sub}\n\n{msg}")
+    s.quit()
+
+    
+
+if __name__ == "__main__":
+    #sendEmail(GMAIL_ID, "subject", "test message")
+    
+    df = pd.read_excel("Data.xlsx")
+    #print(df)
+    today = datetime.datetime.now().strftime("%d-%m")
+    yearNow = datetime.datetime.now().strftime("%Y")
+    #print(type(today))
+    writeInd = []
+
+    for index, item in df.iterrows():
+        #print(index, item['Birthday'])
+        bday = item['Birthday'].strftime("%d-%m")
+        #print(bday)
+        if(today == bday) and yearNow not in str(item['Year']):
+            sendEmail(item['Email'], "Happy Birthday", item['Dialogue'])
+            writeInd.append(index)
+
+    #print(writeInd)
+    for i in writeInd:
+        yr= df.loc[i, 'Year']
+        df.loc[i, 'Year']= str(yr) + ',' + str(yearNow)
+        #print(df.loc[i, 'Year'])
+        
+#print(df)
+df.to_excel('Data.xlsx', index=False)
